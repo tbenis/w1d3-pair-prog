64b8800b44f3262cd8682c01eb1d76bdbd2ea750
Rick Kunz <rick.c.kunz@gmail.com>
2022-07-13 12:10:59 -0700

    fibCalc part 2

--- src/main/java/fib/Fib.java
+++ src/main/java/fib/Fib.java
@@ -4,24 +4,28 @@ public class Fib {
 
     public static void main(String[] args) { //psvm
 //        System.out.println(fibCalc(5));
-        fibCalc(10);
+        System.out.println(fibCalc(7, 21));
+//        sout(fibCalc(5, 2));
     }
 
-    public static int fibCalc(int num){
+    public static boolean fibCalc(int num, int numToCheck){
         int result = 1;
         int prev = 1;
 
         System.out.println(result);
         System.out.println(prev);
-        if(num < 2) return result;
+//        if(num < 2) return result;
 
         for (int i = 2; i < num; i++) { //fori
             int temp = result;
             result += prev;
             prev = temp;
+            if (result == numToCheck) {
+                return true;
+            }
             System.out.println(result);
         }
-
-        return result;
+        return false;
+//        return result;
     }
 }
